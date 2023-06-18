e59bc9ba700ce02160fc091aadc80e2c76f5b151
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-18 22:59:33 +0300

    add cards

--- js/startGame.js
+++ js/startGame.js
@@ -1,4 +1,5 @@
-import { createIconsArray, duplicateArray } from './utils.js';
+import { createGameCard } from './gameCard.js';
+import { createIconsArray, duplicateArray, shuffle } from './utils.js';
 
 export const startGame = (difficult) => {
     let firstCard = null;
@@ -7,5 +8,21 @@ export const startGame = (difficult) => {
     const gameSection = document.querySelector('.game-section__container');
     const gameTable = document.createElement('div');
     const cardsIcons = createIconsArray(difficult);
-    const duplicatedCerdsIcons = duplicateArray(cardsIcons);
+    const duplicatedCardsIcons = duplicateArray(cardsIcons);
+    const restartBtn = document.createElement('button');
+
+    gameSection.innerHTML = '';
+
+    restartBtn.textContent = 'Restart';
+    gameTable.classList.add('game-table');
+    restartBtn.classList.add('restart-btn');
+    shuffle(duplicatedCardsIcons);
+
+    duplicatedCardsIcons.forEach((icon) =>
+        gameTable.append(createGameCard('question-circle', icon))
+    );
+    gameSection.append(gameTable, restartBtn);
+    const cards = document.querySelectorAll('.game-card');
+
+    cards.forEach((card, index) => card.addEventListener('click', () => {}));
 };
